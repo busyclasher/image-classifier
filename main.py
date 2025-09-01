f48ef2b0cc9465afa4e28e1c60768d6d2ab536a2
Cody <you@example.com>
2025-09-01 23:05:24 +0800

    Refactor main.py to remove the main function and implement model loading with MobileNetV2

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from image-classifier!")
+import cv2
+import numpy as np
+import streamlit as st
+from tensorflow.keras.applications.mobilenet_v2 import (
+    MobileNetV2,
+    preprocess_input,
+    decode_predictions
+)
 
+from PIL import image
 
-if __name__ == "__main__":
-    main()
+def load_model():
+    model = MobileNetV2(weights="imagenet")
+    return model
